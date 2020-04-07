6c06303860399acdeb2d92aa80c7fd916bd22c7a
jfcarocota <jfcarocota@gmail.com>
2020-04-07 09:53:19 -0700

    Updates

---
+++ precache-manifest.f24afac28cbff950d32444039976e3cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b548d87a5cc6cdd4bb1a34e2fd2ef896",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "674c4629ccee570b912a",
+    "url": "/covid19/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6843b4eaf37299fa6626",
+    "url": "/covid19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "674c4629ccee570b912a",
+    "url": "/covid19/static/js/2.c2ffab91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19/static/js/2.c2ffab91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6843b4eaf37299fa6626",
+    "url": "/covid19/static/js/main.835507b4.chunk.js"
+  },
+  {
+    "revision": "72a082ea0ecbed87a791",
+    "url": "/covid19/static/js/runtime-main.4701c998.js"
+  }
+]);
