498c3defd36799dd1c792b36efdd9674b9ffdf69
jfcarocota <jfcarocota@gmail.com>
2020-04-07 23:28:32 -0700

    grafica

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component, Fragment} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Chart from 'chart.js';
 
 export default class App extends Component{
 
@@ -9,29 +10,86 @@ export default class App extends Component{
 
     this.state = {
       url: 'https://pomber.github.io/covid19/timeseries.json',
-      date: new Date() - 1,
-      data: {}
+      data: {},
+      context: '',
+      arr: []
     }
+    this.canvas = React.createRef();
+
   }
 
   componentDidMount() {
     const {url} = this.state;
-    /*const response =  fetch(this.state.url);
-    const data =  response.json();
-    const todayData = data.Mexico[data.Mexico.length - 1];
-    console.log(todayData);
-    this.setState({data: todayData});*/
-    fetch(url).then(res => res.json()).then(json => this.setState({data: json.Mexico[json.Mexico.length - 1]}));
-  }
-
-  shouldComponentUpdate(nextProps){
-    return this.state.data !== nextProps.data;
+    fetch(url).then(res => res.json()).then(json => this.setState({
+      data: json.Mexico[json.Mexico.length - 1],
+      arr: json.Mexico
+    }));
+    this.setState({context: this.canvas.getContext('2d')});
   }
 
   render() {
-    const {data} = this.state;
+    const {data, arr} = this.state;
     const {date, confirmed, deaths, recovered} = data;
-    console.log(this.state.data);
+    console.log(this.state);
+
+    const arr_cofirmed = arr.map(e =>{
+      return e.confirmed;
+    });
+
+    const arr_dates = arr.map(e =>{
+      return e.date;
+    });
+
+    const arr_deaths = arr.map(e =>{
+      return e.deaths;
+    });
+
+    const arr_recovereds = arr.map(e =>{
+      return e.recovered;
+    });
+    
+
+
+    const myChart = new Chart(this.state.context, {
+      type: 'line',
+      data: {
+          labels: arr_dates,
+          datasets: [{
+              label: 'Casos confirmados',
+              fill: false,
+              data: arr_cofirmed,
+              backgroundColor: 'rgba(0, 0, 255, 1)',
+              borderColor: 'rgba(0, 0, 255, 255)',
+              borderWidth: 2
+          },
+          {
+            label: 'Fallecimientos',
+            fill: false,
+            data: arr_deaths,
+            backgroundColor: 'rgba(255, 0, 0, 1)',
+            borderColor: 'rgba(255, 0, 0, 255)',
+            borderWidth: 2
+        },
+        {
+          label: 'Pacientes recuperados',
+          fill: false,
+          data: arr_recovereds,
+          backgroundColor: 'rgba(0, 255, 0, 1)',
+          borderColor: 'rgba(0, 255, 0, 255)',
+          borderWidth: 2
+      }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
+
     return (
        <Fragment>
 
@@ -48,6 +106,8 @@ export default class App extends Component{
                 <div className="text-secondary"><strong>Fallecimientos</strong></div>
                 <div className="display-1">{recovered}</div>
                 <div className="text-secondary"><strong>Pacientes recuperados</strong></div>
+                <br/>
+                <canvas id="myChart" ref={c => this.canvas = c} width="400" height="400"></canvas>
                 <br/>
                 <footer>Datos tomados de "The Center for Systems Science and Engineering (CSSE) at JHU", public api: https://github.com/pomber/covid19</footer>
               </div>
